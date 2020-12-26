92bd939a7033d6588afec21fbedd7f18cd0b8e75
ProAdmin007 <rubenweijer@gmail.com>
2020-12-26 20:42:17 +0100

    Basic optellen en delen

--- Testing.py
+++ Testing.py
@@ -1,3 +1,17 @@
 #Dit is een test porjectje voor een simpele calculator
 
-print("dit is een tetje")
+print("----------------------")
+print("THE BEST CALCULATOR!!!")
+print("----------------------")
+
+getal1 = int(input("Vul hier je getal in: "))
+getal2 = int(input("Vul hier getal 2 in: "))
+
+print(getal1)
+print(getal2)
+
+optelling = getal1 + getal2
+deeling = getal1 / getal2
+
+print("Optelling = ", optelling)
+print("Deeling = ", deeling)
