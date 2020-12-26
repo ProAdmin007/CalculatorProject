196f2af6d49f217dc46eecaa40c9109d63d20e24
ProAdmin007 <rubenweijer@gmail.com>
2020-12-26 20:47:24 +0100

    V0.1 is klaar

--- Calculator.py
+++ Calculator.py
@@ -4,14 +4,18 @@ print("----------------------")
 print("THE BEST CALCULATOR!!!")
 print("----------------------")
 
-getal1 = int(input("Vul hier je getal in: "))
-getal2 = int(input("Vul hier getal 2 in: "))
+getal1 = int(input("Getal 1 graag: "))
+getal2 = int(input("Getal 2 graag: "))
 
 print(getal1)
 print(getal2)
 
 optelling = getal1 + getal2
+min = getal1 - getal2
 deeling = getal1 / getal2
+keer = getal1 * getal2
 
 print("Optelling = ", optelling)
+print("Min = ", min)
+print("Keer = ", keer)
 print("Deeling = ", deeling)
