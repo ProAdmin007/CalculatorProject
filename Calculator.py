8eb14635645c2fa3391431f7ee280a48b56caad9
ProAdmin007 <rubenweijer@gmail.com>
2020-12-28 21:33:33 +0100

    If jes toegevoegd en comments ge add

--- Calculator.py
+++ Calculator.py
@@ -4,18 +4,35 @@ print("----------------------")
 print("THE BEST CALCULATOR!!!")
 print("----------------------")
 
+berekening = input("Wat voor berekening wil je doen? ")
+
+plus = "+"
+minn = "-"
+keer = "*"
+gedeeld = "/"
+
 getal1 = int(input("Getal 1 graag: "))
 getal2 = int(input("Getal 2 graag: "))
 
-print(getal1)
-print(getal2)
-
+#reken gedeelte
 optelling = getal1 + getal2
-min = getal1 - getal2
+aftreking = getal1 - getal2
+vermenigvuldiging = getal1 * getal2
 deeling = getal1 / getal2
-keer = getal1 * getal2
 
-print("Optelling = ", optelling)
-print("Min = ", min)
-print("Keer = ", keer)
-print("Deeling = ", deeling)
+if berekening == plus:
+    print("Uitkomst plus: ", optelling)
+elif berekening == minn:
+    print("Uitkomst min: ", aftreking)
+elif berekening == keer:
+    print("Uitkomst keer: ", vermenigvuldiging)
+elif berekening == gedeeld:
+    print("Uitkomst gedeeld: ", deeling)
+else:
+    print("Dat is niet een van de onderstuunde karakters: + - * /")
+
+#print gedeelte
+#    print("Optelling = ", optelling)
+#    print("Min = ", min)
+#    print("Keer = ", keer)
+#    print("Deeling = ", deeling)
